3db8e7cc6990de34a0597eb34109e61e2ea133c1
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-03-29 10:50:01 -0400

    added all function declarations

--- MaxHeap/src/MaxHeap.java
+++ MaxHeap/src/MaxHeap.java
@@ -5,7 +5,47 @@ public class MaxHeap {
     private int size;
     private int numelements;
 
-    
+    // CONSTRUCTORS
 
+    public MaxHeap(int n){
+        size = n;
+        numelements =0;
+        nodes = new Integer[n];
+    }
+
+    public MaxHeap(Integer[] someArray){
+
+    }
+
+    // PUBLIC METHODS
+
+    public void insert(int n){
+        // insert the integer n into the heap properly
+
+    }
+
+    private int deleteMax(){
+        // delete the maximum element in the heap (i.e. the root) and return it,
+        // then replace with the appropriate next element
+    }
+
+    public String toString(){
+        // return a string representation of the heap in level order
+    }
+
+    public static void heapsort(Integer[] arrayToSort){
+        // sort arrayToSort by making into a heap and then
+        // delete max repeatedly and then copy back into the array
+    }
+
+    // ACCESSORS
+
+    public int getSize (){
+        return this.size;
+    }
+    
+    public int getNumelements(){
+        return this.numelements;
+    }
 
 }
